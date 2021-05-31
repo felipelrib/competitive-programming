7d6f41af6721c6ed6bab23210e8ffb6ac16f535d
Felipe Ribeiro <felipelrib@gmail.com>
2021-05-31 15:57:16 -0300

    Project setup and first problem solved

---
+++ URI/1001.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+
+using namespace std;
+
+#define ll long long
+
+#define INF 0x3f3f3f3f
+
+int main(){ _
+    int a, b;
+    cin >> a >> b;
+    cout << "X = " << a + b << endl;
+    return 0;
+}
---
+++ base.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+
+using namespace std;
+
+#define ll long long
+
+#define INF 0x3f3f3f3f
+
+int main(){ _
+    cout << "asdasd";
+    return 0;
+}
